2be5ffba0a84b21efa125354b304453825b56a9d
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-04-21 02:08:01 +0500

    front end for the main page right container done

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -5,40 +5,40 @@ import './MainPage.css'
 function MainPage() {
     return (
         <div className="mainPage">
-        
-        <div className="container">
-{/* left Container */}
-        <div className="leftContainer">
 
-            <div className="ellipseMain"></div>
-            <div className="chats" >
-                chats
-            </div>
-            <div className="currencies">
-                <CurrencyComponent/>
-            </div>
+            <div className="container">
+                {/* left Container */}
+                <div className="leftContainer">
 
+                    <div className="ellipseMain"></div>
+                    <div className="chats" >
+                        chats
             </div>
+                    <div className="currencies">
+                        <CurrencyComponent />
+                    </div>
 
-{/*Mid Container  */}
-            <div className="midContainer">
-                <div className="midContainer__heading">
-                   
-                <div className="midContainer__headingElipse"> </div>
                 </div>
-                <div className="midContainer__body">
-                    body
+
+                {/*Mid Container  */}
+                <div className="midContainer">
+                    <div className="midContainer__heading">
+
+                        <div className="midContainer__headingElipse"> </div>
+                    </div>
+                    <div className="midContainer__body">
+                        body
                 </div>
-                <div className="midContainer__footer">
-                    <div className="midContainer__footerInput">
-                    <input placeholder="write something..." />
+                    <div className="midContainer__footer">
+                        <div className="midContainer__footerInput">
+                            <input placeholder="write something..." />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-{/* Right Container */}
-            <div className="rightContainer">
-                <div className="rightContainer__configureMsg">
+                {/* Right Container */}
+                <div className="rightContainer">
+                    <div className="rightContainer__configureMsg">
                         <div className="rightContainer__configureMsg__heading">
                             Configure Message
                         </div>
@@ -48,23 +48,43 @@ function MainPage() {
                                 read reward
                             </div>
                             <div className="rightContainer__readReward__input">
-                                <input/>
+                                <input />
                             </div>
 
                         </div>
+
+                        <div className="rightContainer__readReward">
+                            <div className="rightContainer__readReward__heading">
+                                reply reward
+                            </div>
+                            <div className="rightContainer__readReward__input">
+                                <input />
+                            </div>
+                        </div>
+
+                        <div className="rightContainer__unlockReward">
+                            <div className="rightContainer__unlockReward__heading">
+                                unlock reward
+                            </div>
+                            <div className="rightContainer__unlockReward__input">
+                                <input placeholder="set password" />
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div className="rightContainer__send">
+                        <div className="rightContainer__send__button">Send</div>
+                    </div>
                 </div>
 
-                <div className="rightContainer__send">
-
-                </div>
-            </div>
-                   
 
 
 
             </div>
-{/* MainPage ends */}
-         </div>
+            {/* MainPage ends */}
+        </div>
     )
 }
 
